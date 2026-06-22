332860afb1a73cbcc0a84c1ee44a4bf4779bde57
Tioyiz <ronaldureta58@gmail.com>
2026-06-22 15:06:49 +0000

    Pending changes exported from your codespace

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ["7rh2m5-5173.csb.app"],
-  },
+  plugins: [react()]
+  // server: {
+   // allowedHosts: ["7rh2m5-5173.csb.app"],
+  //} ,
 });
